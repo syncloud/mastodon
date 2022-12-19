f8c73fff067ba69b64cdfc33ecb447defcdeb270
Boris Rybalkin <ribalkin@gmail.com>
2022-12-19 18:38:04 +0000

    fix node

--- bin/service.streaming.sh
+++ bin/service.streaming.sh
@@ -7,4 +7,4 @@ export RAILS_ENV=production
 export SOCKET=/var/snap/mastodon/common/web.socket
 export LD_PRELOAD=libjemalloc.so
 export STREAMING_CLUSTER_NUM=1
-exec $DIR/node/bin/node ./streaming
+exec $DIR/node/bin/node.sh ./streaming
