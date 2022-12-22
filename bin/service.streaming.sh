fd61977d05e112dc2d8ad87592e239de63fad591
Boris Rybalkin <ribalkin@gmail.com>
2022-12-22 18:28:27 +0000

    no streaming for now

--- bin/service.streaming.sh
+++ bin/service.streaming.sh
@@ -3,8 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 cd $DIR/ruby/mastodon
-. /var/snap/mastodon/current/config/.env.production
-export RAILS_ENV=production
 export SOCKET=/var/snap/mastodon/common/web.socket
 export STREAMING_CLUSTER_NUM=1
 exec $DIR/node/bin/node.sh ./streaming
