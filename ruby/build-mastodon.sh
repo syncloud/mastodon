c24c71e775049804f2802437694f140374622494
Boris Rybalkin <ribalkin@gmail.com>
2022-12-23 15:22:47 +0000

    npm yarn

--- ruby/build-mastodon.sh
+++ ruby/build-mastodon.sh
@@ -9,7 +9,7 @@ apt install -y git imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file gi
                      g++ libprotobuf-dev protobuf-compiler pkg-config gcc autoconf \
                      bison build-essential libssl-dev libyaml-dev libreadline6-dev \
                      zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev \
-                     libidn11-dev libicu-dev libjemalloc-dev nodejs
+                     libidn11-dev libicu-dev libjemalloc-dev nodejs npm
 
 export PATH=$PATH:/current/bin
 cd /mastodon
@@ -17,9 +17,10 @@ cd /mastodon
 bundle config deployment 'true'
 bundle config without 'development test'
 bundle install -j$(getconf _NPROCESSORS_ONLN)
+npm install --global yarn
 RAILS_ENV=production OTP_SECRET=1 SECRET_KEY_BASE=2 bundle exec rake assets:precompile
              
-apt remove -y git-core g++ gcc autoconf build-essential
+apt remove -y git-core g++ gcc autoconf build-essential nodejs npm
 apt autoremove -y
 rm -rf /var/lib/apt/lists/*
 
