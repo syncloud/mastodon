fcba0a77b12f142024fff6890ad470d3f7850cfc
Boris Rybalkin <ribalkin@gmail.com>
2022-12-24 17:46:41 +0000

    arm32 dind seccomp fix

--- ruby/build.sh
+++ ruby/build.sh
@@ -5,6 +5,12 @@ cd ${DIR}
 RUBY=$1
 NODE=$2
 BUILD_DIR=${DIR}/../build/snap/ruby
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee -a /etc/apt/sources.list.d/buster-backports.list
+sudo apt update
+sudo apt install -y -t buster-backports libseccomp2
+
 docker build --build-arg RUBY=$RUBY --build-arg NODE=$NODE -t syncloud .
 docker create --name=syncloud syncloud
 mkdir -p ${BUILD_DIR}
