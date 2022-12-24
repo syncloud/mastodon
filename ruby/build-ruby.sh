9517d34cbc8682f0d2bb5bf73885a09bffbfaa9c
Boris Rybalkin <ribalkin@gmail.com>
2022-12-24 16:49:08 +0000

    clean ruby build dir

--- ruby/build-ruby.sh
+++ ruby/build-ruby.sh
@@ -13,12 +13,11 @@ apt install -y gnupg2 curl procps
 command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 
-
 curl -sSL https://get.rvm.io | bash -s stable --path ${PREFIX}
 source ${PREFIX}/scripts/rvm
 rvm install ${VERSION} --movable
 
 mv /build/rubies/ruby-* /current
-
+rm -rf /build
 rm /etc/rvmrc
 rm /etc/profile.d/rvm.sh
