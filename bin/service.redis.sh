94c80d7c9e1827624efecd3e01794c1812efcc03
Boris Rybalkin <ribalkin@gmail.com>
2022-12-25 16:44:47 +0000

    fix redis

--- bin/service.redis.sh
+++ bin/service.redis.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec ${DIR}/redis/bin/redis-server /var/snap/mastodon/current/config/redis.conf
+exec ${DIR}/redis/bin/redis-server.sh /var/snap/mastodon/current/config/redis.conf
--- bin/service.streaming.sh
+++ bin/service.streaming.sh
@@ -5,4 +5,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd $DIR/ruby/mastodon
 export SOCKET=/var/snap/mastodon/common/web.socket
 export STREAMING_CLUSTER_NUM=1
+while diff /snap/mastodon/current/version /var/snap/mastodon/current/version; do
+    echo "waiting for db"
+    sleep 2
+done
 exec $DIR/ruby/bin/node.sh ./streaming
