e80b0cbf35a66354e49ac3cbea1a9177f7a52278
Boris Rybalkin <ribalkin@gmail.com>
2022-12-26 15:46:00 +0000

    fix nginx default error log

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 /bin/rm -f /var/snap/mastodon/common/web.socket
-exec ${DIR}/nginx/sbin/nginx -c /snap/mastodon/current/config/nginx.conf -p ${DIR}/nginx -g 'error_log syslog:server=unix:/dev/log warn;'
+exec ${DIR}/nginx/sbin/nginx -c /snap/mastodon/current/config/nginx.conf -p ${DIR}/nginx -e stderr
