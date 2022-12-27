e112512ebac8ff3fb3eec5dd3d6085ffcf79d492
Boris Rybalkin <ribalkin@gmail.com>
2022-12-27 12:17:13 +0000

    fix publish

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -34,8 +34,9 @@ def test_login(selenium, device_user, device_password):
 
 def test_publish(selenium):
     selenium.find_by_xpath("//span[text()='Publish']").click()
-    selenium.find_by_id("message").send_keys("test post")
+    selenium.find_by_xpath("//label/textarea").send_keys("test post")
+    selenium.find_by_xpath("//button[text()='Publish!']").click()
+    selenium.find_by_xpath("//*[text()='test post']")
 
 def test_teardown(driver):
     driver.quit()
-
