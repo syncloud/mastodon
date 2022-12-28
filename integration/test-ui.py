de8bad370a94efe3efc5e7935a789f2961081c1e
Boris Rybalkin <ribalkin@gmail.com>
2022-12-28 16:35:09 +0000

    mobile fix

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -33,7 +33,9 @@ def test_login(selenium, device_user, device_password):
     lib.login(selenium, device_user, device_password)
 
 def test_publish(selenium):
-    selenium.find_by_xpath("//span[text()='Done']").click()
+    done = "//span[text()='Done']"
+    if selenium.exists_by(By.XPATH, done)
+        selenium.find_by_xpath(done).click()
     selenium.find_by_xpath("//span[text()='Publish']").click()
     selenium.find_by_xpath("//label/textarea").send_keys("test post")
     selenium.find_by_xpath("//button[text()='Publish!']").click()
