039e0ec9f1b196c63f682686d35a9cc333fb34f0
Boris Rybalkin <ribalkin@gmail.com>
2023-01-03 14:39:00 +0000

    export

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -112,7 +112,7 @@ def test_import(selenium, ui_mode):
 def test_export(selenium, ui_mode):
     selenium.find_by_xpath("//a[@title='Preferences']").click()
     selenium.find_by_xpath("//a[contains(.,'Import and export')]").click()
-    selenium.find_by_xpath("//a[contains(.,'Data export']").click() 
+    #selenium.find_by_xpath("//a[contains(.,'Data export']").click() 
     selenium.screenshot('export')
     if ui_mode == "mobile":
         selenium.find_by_xpath("//a[@aria-label='Toggle menu']").click()
