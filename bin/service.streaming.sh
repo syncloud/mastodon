99b092aa8b7052c6a514186e9e6ba1a9f0ead216
Boris Rybalkin <ribalkin@gmail.com>
2023-12-08 09:36:44 +0000

    remove socket before start

--- bin/service.streaming.sh
+++ bin/service.streaming.sh
@@ -6,5 +6,6 @@ cd $DIR/ruby/mastodon
 export SOCKET=/var/snap/mastodon/current/streaming.socket
 export STREAMING_CLUSTER_NUM=1
 export NODE_ENV=production
+/bin/rm -rf $SOCKET
 $DIR/bin/wait-for-db.sh
 exec $DIR/ruby/bin/node.sh ./streaming
