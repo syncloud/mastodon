edf3e1d25f0661ab635ddcff94a5fb2691e6aada
Boris Rybalkin <ribalkin@gmail.com>
2024-02-24 09:11:23 +0000

    direct run

--- bin/service.cleaner.sh
+++ bin/service.cleaner.sh
@@ -3,11 +3,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 $DIR/bin/wait-for-db.sh
 while true; do
   echo "cleaning"
-  snap run mastodon.tootctl media remove --prune-profiles --include-follows --days 1
-  snap run mastodon.tootctl preview_cards remove --days 1
-  snap run mastodon.tootctl statuses remove --days 1
-  snap run mastodon.tootctl accounts cull
-  snap run mastodon.tootctl accounts prune
+  $DIR/tootctl.sh media remove --prune-profiles --include-follows --days 1
+  $DIR/tootctl.sh preview_cards remove --days 1
+  $DIR/tootctl.sh statuses remove --days 1
+  $DIR/tootctl.sh accounts cull
+  $DIR/tootctl.sh accounts prune
   echo "sleeping for 24h"
   sleep 24h
 done
