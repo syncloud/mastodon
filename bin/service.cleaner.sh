9f00cb5fcabd942fb08c6184ab09292d5af927a8
Boris Rybalkin <ribalkin@gmail.com>
2024-03-02 18:24:51 +0000

    retain 10 days

--- bin/service.cleaner.sh
+++ bin/service.cleaner.sh
@@ -3,9 +3,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/wait-for-db.sh
 while true; do
   echo "cleaning"
-  $DIR/tootctl.sh media remove --prune-profiles --include-follows --days 1
-  $DIR/tootctl.sh preview_cards remove --days 1
-  $DIR/tootctl.sh statuses remove --days 1
+  $DIR/tootctl.sh media remove --prune-profiles --include-follows --days 10
+  $DIR/tootctl.sh preview_cards remove --days 10
+  $DIR/tootctl.sh statuses remove --days 10
   $DIR/tootctl.sh accounts cull
   $DIR/tootctl.sh accounts prune
   echo "sleeping for 24h"
