a7fe225fd086ad60c2c5b0d86d73d9c9a1165853
Boris Rybalkin <ribalkin@gmail.com>
2025-10-23 19:11:06 +0100

    more ls

--- redis/test.sh
+++ redis/test.sh
@@ -3,4 +3,7 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/redis
+ls -la $BUILD_DIR
+ls -la $BUILD_DIR/lib/
+ls -la $BUILD_DIR/lib/*-linux*/ld-*.so.*
 $BUILD_DIR/bin/redis.sh -v
