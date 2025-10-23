29425a7a19397e21f2916ec42a4b01f5a5510f89
Boris Rybalkin <ribalkin@gmail.com>
2025-10-23 16:23:24 +0100

    redis lib

--- redis/build.sh
+++ redis/build.sh
@@ -5,5 +5,7 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/redis
 mkdir -p $BUILD_DIR
 cp -r /usr ${BUILD_DIR}
-cp -r /lib ${BUILD_DIR}
+cd ${BUILD_DIR}
+ln -s usr/lib lib
+cd $DIR
 cp -r ${DIR}/bin ${BUILD_DIR}/bin
