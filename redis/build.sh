ccbd6775832937e4b26477800641414f77601a5d
Boris Rybalkin <ribalkin@gmail.com>
2025-10-23 20:03:48 +0100

    fix

--- redis/build.sh
+++ redis/build.sh
@@ -5,19 +5,5 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/redis
 mkdir -p $BUILD_DIR
 cp -r /usr ${BUILD_DIR}
-
-ls -la /lib/*-linux*
-ls -la /usr/lib/*-linux*
-
-ls -la /usr/lib/*-linux*/ld-*.so.*
-ls -la /lib/*-linux*/ld-*.so.*
-
-cd ${BUILD_DIR}
-ln -s usr/lib lib
-ls -la
-ls -la lib/
-ls -la usr/lib/*-linux*/ld-*.so.*
-ls -la lib/*-linux*/ld-*.so.*
-
-cd $DIR
+cp -r /lib ${BUILD_DIR}
 cp -r ${DIR}/bin ${BUILD_DIR}/bin
--- redis/test.sh
+++ redis/test.sh
@@ -3,8 +3,4 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/redis
-ls -la $BUILD_DIR
-ls -la $BUILD_DIR/lib/
-ls -la $BUILD_DIR/lib/*-linux*/
-ls -la $BUILD_DIR/lib/*-linux*/ld-*.so.*
 $BUILD_DIR/bin/redis.sh -v
