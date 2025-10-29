16ef8d59d6143d3506c16d50bd1dbb55b8854bf4
Boris Rybalkin <ribalkin@gmail.com>
2025-10-29 20:50:23 +0000

    no rails test

--- ruby/test.sh
+++ ruby/test.sh
@@ -7,6 +7,6 @@ find . -name bundler
 ./bin/ruby.sh -e "puts $:"
 ./bin/ruby.sh -e 'puts Gem.path'
 ./bin/ruby.sh -e "require 'bundler/setup'"
-./bin/ruby.sh mastodon/bin/rails
+#./bin/ruby.sh mastodon/bin/rails
 ./bin/node.sh -e 'console.log("test")'
 ./bin/file -b --mime $DIR/../test/csv/following.csv
