be66f7ec8ffe35d3a169702db16d5c2b800a0fcd
Boris Rybalkin <ribalkin@gmail.com>
2025-10-29 15:36:17 +0000

    libs

--- ruby/bin/ruby.sh
+++ ruby/bin/ruby.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/local/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/ruby/*.*.*/*-linux-*)
 #export LD_PRELOAD=$(readlink -f ${DIR}/usr/lib/*-linux-gnu*/libjemalloc.so)
-export GEM_HOME="$(echo $DIR/usr/lib/ruby/gems/*/)"
+export GEM_HOME="$(echo $DIR/usr/lib/ruby/gems/*.*.*)"
 export GEM_PATH="$GEM_HOME"
 export PATH="$GEM_HOME/bin:$PATH"
-export RUBYLIB="$(echo $DIR/usr/lib/ruby/*/*-linux-*)"
+RUBYLIB="$(echo $DIR/usr/lib/ruby/*.*.*)"
+export RUBYLIB="$RUBYLIB:$(echo $DIR/usr/lib/ruby/*.*.*/*-linux-*)"
 exec ${DIR}/lib*/ld-*.so* --library-path $LIBS ${DIR}/usr/bin/ruby "$@"
