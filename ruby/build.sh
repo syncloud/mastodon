a6a5f333809234f3d19d0bb02e06c9f5d3cfddc3
Boris Rybalkin <ribalkin@gmail.com>
2025-10-29 10:24:55 +0000

    log

--- ruby/build.sh
+++ ruby/build.sh
@@ -54,6 +54,8 @@ build-base \
 npm \
 git
 
+ruby -e 'puts Gem.path'
+
 ln -s /var/snap/mastodon/current/config/.env.production .env.production
 sed -i '1s@^@#!/snap/mastodon/current/ruby/bin/ruby.sh\n@' /usr/bin/bundle
 sed -i '1s@^@#!/snap/mastodon/current/ruby/bin/ruby.sh\n@' vendor/bundle/ruby/*/bin/*
